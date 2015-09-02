cd62a9a5aaed426f16529b550697382b504e275e
Martin Sandström <martin@marteinn.se>
2015-09-02 22:22:53 +0200

    Updated so test suite works

--- tests/test_firefox.py
+++ tests/test_firefox.py
@@ -7,12 +7,12 @@ class TestFirefox(unittest.TestCase):
         self.driver = webdriver.Firefox()
 
     def test_title(self):
-        self.driver.get("http://tomwaits.com")
+        self.driver.get("http://www.tomwaits.com")
         self.assertEquals("Tom Waits", self.driver.title)
 
     def test_url(self):
         self.driver.get("http://tomwaits.com")
-        self.assertEquals(self.driver.current_url, "http://tomwaits.com/")
+        self.assertEquals(self.driver.current_url, "http://www.tomwaits.com/")
 
     def tearDown(self):
         self.driver.quit()
